5e94d4a0a410a237c138262872b86c2433bb2e5e
osman-butt <butt.osman@gmail.com>
2023-10-13 01:45:02 +0200

    Moved controller to app.js

--- app.js
+++ app.js
@@ -1,10 +1,55 @@
-import { MusicController } from "./js/controller/musicController.js";
+import { RestAPI } from "./js/utility/restApi.js";
+import { ListRenderer } from "./js/view/listRenderer.js";
+import { ArtistRenderer } from "./js/view/artistRenderer.js";
+import { ArtistUpdateDialog } from "./js/view/artistUpdateDialog.js";
 
 window.addEventListener("load", initApp);
 
+// Model
+let artistList = [];
+let songList = [];
+let albumList = [];
+
+// View
+let updateDialog = null;
+
 async function initApp() {
   console.log("App.js is running 🎉");
-  const controller = new MusicController();
-  await controller.setDataLists();
-  controller.renderViews();
+  await initViews();
 }
+
+// CONTROLLER FUNCTIONS
+async function initViews() {
+  await getData();
+  // Create list component and render
+  const artistsView = new ListRenderer(
+    artistList,
+    "#artists-container",
+    ArtistRenderer
+  );
+  artistsView.render();
+
+  // Create dialogs and render
+  updateDialog = new ArtistUpdateDialog("update-dialog");
+  updateDialog.render();
+}
+
+async function getData() {
+  const api = new RestAPI();
+  await api.getDataLists();
+  artistList = api.artistList;
+  songList = api.songList;
+  albumList = api.albumList;
+}
+
+function selectArtistForUpdate(artist) {
+  updateDialog.setArtist(artist);
+  updateDialog.show();
+}
+
+async function updateArtist(artist) {
+  await RestAPI.updateArtist(artist);
+  await initViews();
+}
+
+export { selectArtistForUpdate, updateArtist };
