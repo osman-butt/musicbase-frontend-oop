8d237f3c3c55412786aa05f301604b3678068585
osman-butt <butt.osman@gmail.com>
2023-10-12 22:01:12 +0200

    Create listRenderer.js

---
+++ js/view/listRenderer.js
@@ -0,0 +1,33 @@
+export default class ListRenderer {
+  constructor(list, container, itemRenderer) {
+    this.itemRenderer = itemRenderer;
+    this.container = document.querySelector(container);
+    this.setList(list);
+  }
+
+  setList(list) {
+    // Build list of renderers with items in them
+    this.items = list.map(item => new this.itemRenderer(item));
+  }
+
+  clear() {
+    this.container.innerHTML = "";
+  }
+
+  render() {
+    this.clear();
+
+    // create a filtered list to render
+    const filteredList = this.list;
+
+    for (const itemRenderer of filteredList) {
+      const html = itemRenderer.render();
+      this.container.insertAdjacentHTML("beforeend", html);
+
+      if (itemRenderer.postRender) {
+        const element = this.container.lastElementChild;
+        itemRenderer.postRender(element);
+      }
+    }
+  }
+}
