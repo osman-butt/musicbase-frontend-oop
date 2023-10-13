2c2ff3c6f409dd6f21153baa0b4367e1ef85064f
osman-butt <butt.osman@gmail.com>
2023-10-13 21:26:05 +0200

    Added update

--- js/view/artistRenderer.js
+++ js/view/artistRenderer.js
@@ -4,19 +4,21 @@ export class ArtistRenderer extends ItemRenderer {
   render() {
     const artist = this.item;
     const html = /*html*/ `
-    <article class="artist-item" data-action='show'>
-        <img src=${artist?.artistImage ? artist?.artistImage : ""} alt="">
-        <p>${artist.artistName}</p>
+    <article class="artist-item" data-action=''>
+        <img src=${
+          artist?.artistImage ? artist?.artistImage : ""
+        } alt="" data-action='update'>
+        <p data-action='update'>${artist.artistName}</p>
     </article>`;
     return html;
   }
   postRender(element) {
     // Add eventListener to element
     element.addEventListener("click", event => {
-      const action = event.target.dataset.action ?? "update";
+      const action = event.target.dataset.action;
       const artist = this.item;
       // Handle action - as defined in data-action="..."
-      if (action == "update") {
+      if (action === "update") {
         // ask controller to start update view
         controller.selectArtistForUpdate(artist);
       }
